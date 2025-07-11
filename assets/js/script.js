7e23fe527235fea80295d86c1d60efdfe80f9ad9
Bianca <99222703+BiancaADJ@users.noreply.github.com>
2025-07-11 17:18:06 -0300

    Update 11-07-25

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function login(n){
+  if(n === 1){ // Login
+
+  }else if(n === 2){ // Register
+  
+  }else if(n === 3){ // Forgot Password
+
+  }else{ // Default case, do nothing or show an error
+
+  }
+}
+
+function togglePasswordVisibility() {
+
+}
