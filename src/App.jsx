047754c2a30564c1d79ad3a140b6b03af302d55a
Bianca Araujo Coelho <99222703+BiancaADJ@users.noreply.github.com>
2026-08-06 21:05:41 -0300

    Update 06.08.26

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,62 @@ function App(){
 
   return ( /* Página */
     <><html>
-      <body>
-      <header className="header App-header">
-        <div className=""><img src={logo} alt="Logo"/></div>
-        <div className=""><img src={lupa} alt="lupa" /><input type="text"/></div>
-        <div className="">
-          <i><img src={moeda} alt="moeda"/><output>0.000.000,00</output></i>
-          <img src={user} alt="user"/>
+      <body className='border'>
+      <header className='header App-header flex-line'>
+        <div className='line-center'><img src={logo} alt='Logo'/></div>
+        <div className='line-center'>
+          <div className='relative border'>
+            <img src={lupa} alt='lupa'/>
+            <input className='relative' type='text'/></div>
+          </div>
+        <div className='line-center'>
+          <i className='line-center'><img src={moeda} alt='moeda'/><output>0.000.000,00</output></i>
+          <img src={user} alt='user'/>
         </div>
       </header>
 
+      <section>
+        <output><h2>Reparte</h2></output>
+        <output><h1>0.000.000,00</h1></output>
+        <p>Se sobra, acumula. Se falta, reparte.</p>
+      </section>
+
+      <section>
+        <div className='flex-line'>
+          <div className='column-center'>
+            <h3>1</h3>
+            <img src={moeda} alt='moeda'/>
+          </div>
+          <div className='column-center'>
+            <h3>2</h3>
+            <img src={moeda} alt='moeda'/>
+          </div>
+          <div className='column-center'>
+            <h3>3</h3>
+            <img src={moeda} alt='moeda'/>
+          </div>
+        </div>
+        <div className='flex-line'>
+          <div className='column-center'>
+            <h3>1</h3>
+            <img src={moeda} alt='moeda'/>
+          </div>
+          <div className='column-center'>
+            <h3>2</h3>
+            <img src={moeda} alt='moeda'/>
+          </div>
+          <div className='column-center'>
+            <h3>3</h3>
+            <img src={moeda} alt='moeda'/>
+          </div>
+        </div>
+      </section>
+
       <section></section>
+
+      <footer>
+        <p>footer</p>
+      </footer>
       </body></html></>
   )
 }
